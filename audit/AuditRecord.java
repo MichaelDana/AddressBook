162d88f37ed4f64983e90dfb57b21b3323df1eb6
Andres Rivera <andresrivera@sandiego.edu>
2020-04-30 19:56:12 -0700

    Added command field to AuditRecord.java

--- audit/AuditRecord.java
+++ audit/AuditRecord.java
@@ -17,15 +17,16 @@ class AuditRecord implements AuditRecordInterface{
         mUserID = other.mUserID;
 		mCommand = other.mCommand
     }
-	public AuditRecord(AuditType type, String userID)
+	public AuditRecord(AuditType type, String command, String userID)
 	{
 		mDate = LocalDate.now();
 		mTime = LocalTime.now();
 		mType = type;
 		mUserID = userID;
+		mCommand = command;
 	}
 	public void display()
-	{	System.out.println(mDate+" "+mTime+" "+mType+" "+mUserID);	
+	{	System.out.println(mDate+" "+mTime+" "+mType+" "+mCommand+" "+mUserID);	
 	}
 	public String getKey()
 	{	return mUserID;
