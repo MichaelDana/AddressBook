3e5830d5bfdb828437f9303a1ffc1144a495a7a1
Andres Rivera <andresrivera@sandiego.edu>
2020-04-30 22:42:54 -0700

    Added RecordInterface and updated AuditRecord Interface to implement RecordInterface

---
+++ RecordInterface.java
@@ -0,0 +1,4 @@
+interface RecordInterface
+{
+	
+}
--- audit/AuditRecordInterface.java
+++ audit/AuditRecordInterface.java
@@ -1,6 +1,6 @@
 //package audit;
 
-interface AuditRecordInterface
+interface AuditRecordInterface extends RecordInterface
 {
     enum AuditType 
 	{	LF, LS, L1, LO, SPC, FPC, AU, DU
