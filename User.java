be6da636a80c95f5e36815e7806a37f65b7dd3d1
Jordan <jmatuszewski@sandiego.edu>
2020-04-30 19:37:06 -0700

    User object created.

---
+++ User.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.util.HashMap;
+
+
+public Class User{
+	private String myUserId;
+	private String myPassword;
+
+	public User(String userId, String password){
+
+		myUserId= userId;
+		myPassword= password;
+		}
+
+	public static User createUserFromArgs(String UserId){
+
+		String myUserId = "";
+		String myPassword = "";
+
+		//parse args
+		return new User(userId, password);
+		}
+	
+	public String getUserId() {
+		return this.myUserId;
+		}
+
+	public void setUserId(String UserId) {
+		this.myUserId = UserId;
+		}	
+
+	public String getPassword() {
+		return this.myPassword;
+		}
+
+}
