366221f6a094feea51214de5f8cd1cc6d0b42a1d
Jordan <jmatuszewski@sandiego.edu>
2020-04-30 19:41:51 -0700

    UserModule updated.

--- UserModule.java
+++ UserModule.java
@@ -1,38 +1,9 @@
-public class User{
-        private static final int MAX_NUMBER_OF_USERS = 256;
-        private final HashMap<String,User> users;
-        private int numberOfUsers;
-
-        public class User
-	{
-	private String myUserId;  
-	private String myPassword;
-		
-	public User(String userId, String password){
-  
-		myUserId= userId;
-		myPassword= password;
-	}
-
-	public static User createUserFromArgs(List<String> args){
-		String myUserId = "";  
-		String myPassword = "";
-
-		//parse args
-		myUserId = args.remove(0);
-			
-		}
-        return new User(userId, password);
-	}
-	
+public class UserModule{
+        
         public void ADU(String UserId) throws RuntimeException
         {
 
-                if(numberOfUsers < MAX_NUMBER_OF_USERS)
-                {
-
-                        users.put(User.myId, user);
-                        numberOfUsers++;
+		{			
                         System.out.println("USER ADDED");
                 }
                 else
