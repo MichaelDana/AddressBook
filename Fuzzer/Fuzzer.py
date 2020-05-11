b44d7d5b0fae02a185e06a355ddece0960dd9738
unknown <michael@danafive.com>
2020-05-10 22:52:45 -0700

    Update fuzzer

--- Fuzzer/Fuzzer.py
+++ Fuzzer/Fuzzer.py
@@ -1,34 +1,50 @@
 import subprocess
-import pexpect
+from pexpect import popen_spawn
+from pexpect import TIMEOUT as expectTimeout
+
 
 def getInputStr(testCase):
     inputStr = ""
     for testCaseCommand in testCase:
         inputStr += testCaseCommand + "\n"
 
-    return inputStr 
+    return inputStr
+
 
 testCases = {
-    "login as admin" : ["LIN admin", "test", "test", "EXT"]
+    "Login as admin, first time": [
+        [
+            "LIN admin",
+            "This is the first time the account is being used. Create a new password. Passwords may contain up to 24 upper-or lower-case letters or numbers. Choose an uncommon password that would be difficult to guess.12341"
+        ],
+        [
+            "test",
+            "Reenter the same password: "
+        ],
+        [
+            "test",
+            "OK"
+        ],
+        [
+            "EXT"
+        ]
+    ]
     # "login as admin, hlp" : ["LIN admin", "test", "HLP", "EXT"]
     # "hlp" : ["HLP", "HLP", "HLP", "EXT"]
 }
 
-command = "java -jar Fuzzer/AddressBook.jar"
-for testCase in testCases: 
-    testCaseCommands = testCases[testCase]
-    # lazy use of universal_newlines to prevent the need for encoding/decoding
-    # p = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True, universal_newlines=True)  
-    # output, err = p.communicate(getInputStr(testCaseCommands))
-    # output, err = p.communicate("LIN admin")
-    # stderr is not connected to a pipe, so err is None
-    # print(*testCaseCommands, sep="\n")
-    # print("----")
-    # we just want the result of the command
-    # print(output)
-    # print(output[output.rfind(" "):-1])  # -1 to strip the final newline
-    child = pexpect.spawn ('java -jar Fuzzer/AddressBook.jar')
-    child.expect ('> ')
-    child.sendline ('LIN admin')
-    child.expect("Password: ")
-
+for case in testCases:
+    testCase = testCases[case]
+    child = popen_spawn.PopenSpawn('java -jar AddressBook.jar', timeout=1)
+    child.expect('> ')
+    for step in testCase:
+        child.sendline(step[0])
+        if len(step) > 1:
+            ret = child.expect([step[1], expectTimeout])
+            if(ret == 1):
+                # failure
+                print("Test Failure: " + case )
+                print("Command: " + step[0])
+                print("Expected: " + step[1])
+                print("Actual: " + child.before.decode("utf-8"))
+                break
